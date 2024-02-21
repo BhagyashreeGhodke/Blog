e03f124a5cb204112e9bf63953b83f130117c029
BhagyashreeGhodke <drbpghodke@gmail.com>
2024-02-22 02:31:16 +0530

    create login signup form

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,25 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from './Container/Container'
+import Logo from "./Logo/Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import BtnUI from "./BtnUI/BtnUI";
+import Input from './Input'
+import Login from "./Login";
+import Select from './Select';
+import PostCard from './PostCard'
+
 
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    BtnUI,
+    Input,
+    Login,
+    Select,
+    PostCard
 }
