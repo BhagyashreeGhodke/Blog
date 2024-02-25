d726b7fa3a9587cd2a9a002295894fccac76f3b9
BhagyashreeGhodke <drbpghodke@gmail.com>
2024-02-26 01:21:34 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,38 @@
-import { useState, useEffect } from 'react'
-import { useDispatch} from 'react-redux'
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
-import authService from './appwrite/auth'
-import {login, logout} from './store/authSlice'
-import {Header, Footer} from './components'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-
-  const [loading, setLoading] =useState(true)
+  const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
 
-  useEffect( ()=> {
+  useEffect(() => {
     authService.getCurrentUser()
-    .then( (userData) => {
+    .then((userData) => {
       if (userData) {
         dispatch(login({userData}))
       } else {
         dispatch(logout())
       }
     })
-    .catch( (error) => {
-      console.log("inApp.jsx", error);
-    })
-    .finally( () => setLoading(false) )
+    .finally(() => setLoading(false))
   }, [])
-
-
- 
+  
   return !loading ? (
     <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
       <div className='w-full block'>
         <Header />
         <main>
-
+        TODO:  <Outlet />
         </main>
         <Footer />
       </div>
     </div>
-  )
-  : null
-
+  ) : null
 }
 
 export default App
--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,64 +1,62 @@
 import conf from '../conf/conf.js';
-import { Client, Account, ID } from 'appwrite';
+import { Client, Account, ID } from "appwrite";
 
 
 export class AuthService {
-    client = new Client()
+    client = new Client();
     account;
 
-    constructor(){
+    constructor() {
         this.client
-        .setEndpoint(conf.appwriteUrl)
-        .setProject(conf.appwriteProjectId);
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
-
+            
     }
 
-    //user related services
-    
     async createAccount({email, password, name}) {
         try {
-            const userAccount = await this.account.create(ID.unique(), email, password, name )
-
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
             if (userAccount) {
-                return this.login({email, password})
+                // call another method
+                return this.login({email, password});
             } else {
-                return userAccount
+               return  userAccount;
             }
         } catch (error) {
-            console.log("Appwrite service :: createAccount :: error:", error);
-        };
-
-    }
-
-    async login ({email, password}) {
-        try {
-            return await this.account.createEmailSession( email, password)
-        } catch (error) {
-            console.log("Appwrite service :: login :: error:", error);
+            throw error;
         }
     }
 
-    async getCurrentUser(){
+    async login({email, password}) {
         try {
-            await this.account.get();
+            return await this.account.createEmailSession(email, password);
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error:", error);
+            throw error;
         }
-
-        return null
     }
 
-    async logout(){
+    async getCurrentUser() {
         try {
-            await this.account.deleteSessions()
+            return await this.account.get();
         } catch (error) {
-            console.log("Appwrite service :: logout :: error:", error);
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
         }
     }
 }
 
+const authService = new AuthService();
 
-const authService = new AuthService()
+export default authService
 
-export default authService;
